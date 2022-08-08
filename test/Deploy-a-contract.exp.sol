bf965d25910d64aa3126fb427a36e425bb5fcc8c
finn79426 <finn79426@gmail.com>
2022-08-08 11:25:58 +0800

    Update: Deploy a contract

--- test/Deploy-a-contract.exp.sol
+++ test/Deploy-a-contract.exp.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "./utils.sol";
+import "./Utils.sol";
 
-// Deploy challenge from https://capturetheether.com/challenges/
+// Deploy your challenge contract from https://capturetheether.com/challenges/
 address constant challengeAddr = 0x8705887C2E32eb185d4d472251Cf484B5c04362f;   
+address constant player = 0x5f36131717Cd657e04cA6aEd38D230F72547415C;   // your EOA wallet address
 
 contract ContractTest is Test {
     DeployChallenge challenge = DeployChallenge(challengeAddr);
+    Utils utils = new Utils();
 
     function setUp() public {
         vm.createSelectFork("ropsten");
@@ -16,7 +18,14 @@ contract ContractTest is Test {
     }
 
     function testExploit() public {
-        challenge.isComplete();
+        vm.startPrank(player);
+        
+        /* Write your exploit code  */
+        bool suc = challenge.isComplete();
+        require(suc, "Exploit Failed");
+        /* ------------------------ */
+
+        vm.stopPrank();
     }
 }
 
--- test/utils.sol
+++ test/utils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract utils {
+contract Utils {
     function isDeployed(address _addr) public returns (bool){
         uint32 size;
         assembly { size := extcodesize(_addr) }
