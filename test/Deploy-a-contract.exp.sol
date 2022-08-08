a013928d538aea81a031d7dce00f50ea4065f8bb
finn79426 <finn79426@gmail.com>
2022-08-08 11:38:16 +0800

    Update: Deploy a contract

--- test/Deploy-a-contract.exp.sol
+++ test/Deploy-a-contract.exp.sol
@@ -6,7 +6,6 @@ import "./Utils.sol";
 
 // Deploy your challenge contract from https://capturetheether.com/challenges/
 address constant challengeAddr = 0x8705887C2E32eb185d4d472251Cf484B5c04362f;   
-address constant player = 0x5f36131717Cd657e04cA6aEd38D230F72547415C;   // your EOA wallet address
 
 contract ContractTest is Test {
     DeployChallenge challenge = DeployChallenge(challengeAddr);
@@ -14,18 +13,14 @@ contract ContractTest is Test {
 
     function setUp() public {
         vm.createSelectFork("ropsten");
-        require(utils.isDeployed(challengeAddr), "Invalid challengeAddr");
+        require(utils.isDeployed(challengeAddr), "Challenge not deployed");
     }
 
-    function testExploit() public {
-        vm.startPrank(player);
-        
+    function testExploit() public {        
         /* Write your exploit code  */
         bool suc = challenge.isComplete();
         require(suc, "Exploit Failed");
         /* ------------------------ */
-
-        vm.stopPrank();
     }
 }
 
